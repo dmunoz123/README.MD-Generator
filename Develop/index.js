bbbf9201de2929a3ca42ad2ab17469e970463ef8
Daniel Munoz and Charles Plantemoli <dmunoz7@lion.lmu.edu>
2023-06-15 21:35:43 -0700

    added packages and user questions in questions[]

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const markDown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  { type: "input", name: "username", message: "What is your github username?" },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message:
+      "What would you like to name your GitHub repository? (typically the same as the project title!)",
+  },
+  {
+    type: "input",
+    name: "description",
+    message:
+      "Please provide a short description about your project, i.e., What were your motivations/goals/expected outcomes? What did you learn/accomplish?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:
+      "Please describe the projects installation process. What commands must a user input in order to get this code functioning locally",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "In a few words, describe your projects usage. How will this benefit the user? Provide screenshots if necessary.",
+  },
+  {
+    type: "input",
+    name: "collaborators",
+    message:
+      "Please list any and all collaborators. If you used third-party assets, please cite them here",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message:
+      "Do you want your project to include a license? Please choose an option.",
+    checkbox: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "input",
+    name: "features",
+    message:
+      "If your project includes many different features, please list them here.",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
